49da18fb6c81f4becca0bd27aeda18e1cd611074
jithin8779-art <jithin8779@gmail.com>
2025-09-07 07:11:42 -0700

    Add files via upload

---
+++ assets/main.js
@@ -0,0 +1,85 @@
+let menuIcon = decodeURIComponent.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.ClassList.toggle('fa-xmark');
+    navbar.classList.toggle('active')
+}
+
+
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec =>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop -150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top <offset + height){
+            navLinks.forEach.apply(link =>{
+                link.classList.remove('active');
+                document.querySelector('header nav a[href*='+ id + ']').classList.add('avtive');
+            });
+        };
+    });
+
+
+    let header = document.querySelector('header')
+    header.classList.toggle('sticky' ,  window.scrollY > 100);
+
+    menuIcon.classList.remove('fa-xmark');
+    navbar.classList.remove('active');
+
+};
+ScrollReveal({ 
+    distance: '80px',
+    duration: 2000,
+    delay:200,
+});
+
+ScrollReveal().reveal('.home-content,heading', { dorigin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'button' });
+ScrollReveal().reveal('.home-contact h1, .about-img', { origin:'left' });
+ScrollReveal().reveal('.home-contact p, .about-content', { origin:'right' });
+
+
+const typed = new Typed('multiple-text',{
+    String:['Full Stack Developer', 'Web Developer'],
+    typeSpeed:70,
+    backSpeed:70,
+    backDelay:1000,
+    loop:true,
+});
+
+function sendMail(){
+    var params = {
+        name:document.getElementById("name").value,
+        email:document.getElementById("email").value,
+        message:document.getElementById("message").value,
+    };
+
+const serviceID = "service_amgqy5h";
+const templateID = "template_jsv6zgh"
+
+emailjs
+    .send(serviceID,templateID,params)
+    .then((res) => {
+        document.getElementById("name").value= "";
+        document.getElementById("email").value= "";
+        document.getElementById("message").value= "";
+        console.log(res);
+        alert("your message sent successfully");
+    })
+.catch((err) => console.log(err));
+}
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.querySelector(".hamburger");
+  const navMenu = document.querySelector(".nav-main ul");
+
+  hamburger.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+  });
+})
